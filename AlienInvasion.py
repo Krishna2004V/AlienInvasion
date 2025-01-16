314018ee23ec026473483569e6dbcf680474e4fc
Krishna2004V <mit.vs.krishna@gmail.com>
2025-01-16 21:25:15 +0530

    Creating The Settings Class

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -1,13 +1,16 @@
 import sys
 import pygame
 
+from Settings import Settings
+
 class AlienInvasion:
     """OverAll Class To Manage Game Assets And Behaviour"""
     def __init__(self):
         """Initialize The Game, And Create Game Resources"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.Settings = Settings()
+        self.screen = pygame.display.set_mode((self.Settings.screen_width, self.Settings.screen_height))
         pygame.display.set_caption(("Alien Invasion"))
         #Set The Background Colour
         self.bg_color = (225, 225, 225)
@@ -19,7 +22,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             #Redraw The Screen During Each Pass Through The Loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.Settings.bg_color)
             #Make The Most Recently Drawn Screen Visible
             pygame.display.flip()
             self.clock.tick(60)
---
+++ Settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A Class To Store All The Settings For The Game"""
+    def __init__(self):
+        """Initialize The Game's Settings"""
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (225, 225, 225)
