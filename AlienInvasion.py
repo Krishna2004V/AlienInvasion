594cb905f1d336487454a665dd248cd6f3988365
Krishna2004V <mit.vs.krishna@gmail.com>
2025-01-19 09:42:01 +0530

    Introducing The UpdateScreen Method

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -16,20 +16,21 @@ class AlienInvasion:
         self.Ship = Ship(self)
         #Set The Background Colour
         self.bg_color = (225, 225, 225)
-    def _check_event(self):
+    def _check_events(self):
         #Respond To Keyboard And Mouse Events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+    def _update_screen(self):
+        """Update Images on The Screen, And Flip To The New Screen"""
+        self.screen.fill(self.Settings.bg_color)
+        self.Ship.blitme()
+        pygame.display.flip()
     def run_game(self):
         """Start The Main Loop For The Game"""
         while True:
-            self._check_event()
-            #Redraw The Screen During Each Pass Through The Loop
-            self.screen.fill(self.Settings.bg_color)
-            self.Ship.blitme()
-            #Make The Most Recently Drawn Screen Visible
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
 if __name__ == '__main__':
     #Make A Game Instance, And Run The Game
