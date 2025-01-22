88f59ea8ed5bf85abb5dfd99b33df682d6efb7b8
Krishna2004V <mit.vs.krishna@gmail.com>
2025-01-22 23:42:09 +0530

    Creating A Row of Aliens

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -23,9 +23,17 @@ class AlienInvasion:
         self._create_fleet()
     def _create_fleet(self):
         """Create The Fleet of Aliens"""
-        #Make An Alien
+        #Create An Alien And Keep Adding Aliens Until There's No Room Left
+        #Spacing Between Aliens is One Alien Width
         alien = Alien(self)
-        self.Aliens.add(alien)
+        alien_width = alien.rect.width
+        current_x = alien_width
+        while current_x < (self.Settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.Aliens.add(new_alien)
+            current_x += 2 * alien_width
         #Set The Background Colour
         self.bg_color = (225, 225, 225)
     def _fire_bullet(self):
